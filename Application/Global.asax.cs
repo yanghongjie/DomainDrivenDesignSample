a58d1c89a5c76f8fc34c397db5187be34be56b6b
JohnYang <idotnet@outlook.com>
2015-01-26 17:07:36 +0800

    delete some waring code

--- Application/Global.asax.cs
+++ Application/Global.asax.cs
@@ -15,35 +15,5 @@ namespace Application
         {
             new AppHost().Init();
         }
-
-        protected void Session_Start(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_BeginRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_AuthenticateRequest(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_Error(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Session_End(object sender, EventArgs e)
-        {
-
-        }
-
-        protected void Application_End(object sender, EventArgs e)
-        {
-
-        }
     }
 }
